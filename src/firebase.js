01abe614a92f032c2196549a5250d1d315694c0d
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-09 01:02:35 +0530

    build the header with amazon logo, search bar and the options

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCagfmJWWDn38ANjkJNz0sNQ5aKC7bTBk4",
+  authDomain: "clone-861db.firebaseapp.com",
+  databaseURL: "https://clone-861db.firebaseio.com",
+  projectId: "clone-861db",
+  storageBucket: "clone-861db.appspot.com",
+  messagingSenderId: "179275797017",
+  appId: "1:179275797017:web:ac531d6b67aa328e861088",
+  measurementId: "G-NQV742R08C"
+};
