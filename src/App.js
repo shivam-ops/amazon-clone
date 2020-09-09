594ee6b497a65c4997fbc8204d1a94c7f679a31a
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-09 22:02:26 +0530

    setting up home component and product component. Adding the banner for home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     // BEM
     <div className="app">
       <Header />
-      {/* Header */}
-      {/* Home */}
+      <Home />
     </div>
   );
 }
