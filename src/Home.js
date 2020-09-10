e298160e364f8147048c73e76603d0f59423b942
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-10 15:52:05 +0530

    finishing the product styling

--- src/Home.js
+++ src/Home.js
@@ -14,11 +14,18 @@ function Home() {
 
         <div className="home__row">
           <Product />
+          <Product />
         </div>
 
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
 
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
     </div>
   );
