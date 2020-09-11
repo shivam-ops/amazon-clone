4ad9829fbe560568de7b386df166204eba8b9cc4
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-11 22:44:53 +0530

    setting up checkout page and adding router to link to home page and checkout page

--- src/Home.js
+++ src/Home.js
@@ -25,7 +25,7 @@ function Home() {
             title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
             price={239.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/91gRKbX%2BS8L._AC_SL1500_.jpg"
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
           />
         </div>
 
---
+++ src/Subtotal.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Subtotal.css";
+
+export default class Subtotal extends Component {
+  render() {
+    return <div className="subtotal"></div>;
+  }
+}
