7ad4e9c181fccb44a58987d9b7eb2c5cb6f7f1b3
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-12 11:10:38 +0530

    setting up the subtotal componet. completing the css of subtotal component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
 
-export default class Subtotal extends Component {
-  render() {
-    return <div className="subtotal"></div>;
-  }
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):
+              <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" />
+              This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
+
+export default Subtotal;
