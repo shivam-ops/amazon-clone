19d6f7f3b3a77144555aa55ddfd5b372d55eaaf0
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-22 22:53:24 +0530

    adding css to the payment page

--- src/Payment.js
+++ src/Payment.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -9,6 +10,9 @@ function Payment() {
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket.length} items</Link>)
+        </h1>
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
