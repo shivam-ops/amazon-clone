8d84371aaf214b3139337d4ce426ea2757656f2e
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-24 20:09:05 +0530

    adding more changes to payment page

--- src/Payment.js
+++ src/Payment.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
@@ -10,6 +10,7 @@ import axios from "./axios";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const history = useHistory();
 
   const stripe = useStripe();
   const elements = useElements();
